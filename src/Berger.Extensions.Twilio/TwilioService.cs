c9808d52a1366a58f897c51de27346306300beff
Rafael Cronemberger <rafael@berger.com.br>
2025-01-13 11:05:26 -0300

    First version

---
+++ src/Berger.Extensions.Twilio/TwilioService.cs
@@ -0,0 +1,32 @@
+﻿using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Berger.Extensions.Twilio
+{
+	public class TwilioService
+	{
+		private readonly string _accountSid;
+		private readonly string _authToken;
+		private readonly string _sender;
+
+		public TwilioService(string accountSid, string authToken, string sender)
+		{
+			_accountSid = accountSid;
+			_authToken = authToken;
+			_sender = sender;
+		}
+		public async Task<MessageResource> Send(string receiver, string body)
+		{
+			TwilioClient.Init(_accountSid, _authToken);
+
+			var messageOptions = new CreateMessageOptions(new PhoneNumber(receiver))
+			{
+				From = new PhoneNumber(_sender),
+				Body = body
+			};
+
+			return await MessageResource.CreateAsync(messageOptions);
+		}
+	}
+}
